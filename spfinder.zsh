96b9df55c44b46b767fd7071fe273b720e855c51
RB <rodrigobedmar@gmail.com>
2020-07-10 15:41:51 -0400

    typos corrected

--- spfinder.zsh
+++ spfinder.zsh
@@ -1,21 +1,21 @@
 #!/usr/bin/env zsh
 
 # This script locates and replaces spaces in filenames
-DIR="."
+DIR=$(pwd)
 
 # Controlling a loop with bash read command by redirecting STDOUT
 #  as STDIN to while loop
 #  find will not truncate filenames containing spaces
-find $DIR -type f | while read file; do
+#  f = file d = directory maxdepth self-explanatory
+find $DIR -type f -maxdepth 1 | while read file; do
 
 # Using POSIX class [:space] to find space in filename
 if [[ "$file" = *[[:space:]]* ]]; then
 
-# Substitute space with _ character and consequntly rename the file
+# Substitute space with _ character and consequently rename the file
 mv "$file" `echo $file | tr ' ' '_'`
 fi;
 
-# End of while loop
 done
 
 exit 0
